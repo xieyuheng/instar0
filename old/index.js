0cb1bc6dad12c58ad804dc68719c61263568ce5a
Xie Yuheng <xyheme@gmail.com>
2019-05-17 17:27:11 +0800

    up

--- old/index.js
+++
@@ -1,9 +0,0 @@
-let server = require ("./server")
-let router = require ("./router")
-
-let app_map = new Map ([
-  ["/", require ("./main") .exe],
-  ["/up", require ("./up") .exe],
-])
-
-server.start (3000, router.route, app_map)
